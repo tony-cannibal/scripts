da7e58f2657992ee27751ba69b2e37e7eacc78fd
tony-cannibal <91645702+tony-cannibal@users.noreply.github.com>
2022-07-31 11:04:31 -0500

    Update dmenu-power-runit.sh

--- dmenu-power-runit.sh
+++ dmenu-power-runit.sh
@@ -2,15 +2,19 @@
 
 function powermenu {
 
-    options="Cancel\nShutdown\nRestart"
+    options="Cancel\nShutdown\nRestart\nLogout"
 
-    selected=$(echo -e $options | dmenu -p "Power Menu" )
+    selected=$(echo -e $options | dmenu -p "Power Menu" -fn "terminus-10" )
 
     if [[ $selected = "Shutdown" ]]; then
         loginctl poweroff
 
     elif [[ $selected = "Restart" ]]; then
         loginctl reboot
+    
+    elif [[ $selected = "Logout" ]]; then
+        name=$(whoami)
+        pkill -KILL -u $name       
 
     elif [[ $selected = "Cancel" ]]; then
         return
@@ -18,5 +22,7 @@ function powermenu {
     fi
 
 }
+
+
 
 powermenu
